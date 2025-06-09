951379a4b0ad76a143cdc1a2a9fa96b359e42f20
Barsha Koirala <koiralabarsha024@gmail.com>
2025-06-09 14:54:38 +0545

    learned basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144567
+let accountEmail = "barshakoirala@gmail.com"
+var accountPassword = 2580
+accountCity = "Kathmandu"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+let name = "Barsha"
+let age = 24
+
+
+console.log(typeof "Barsha")
+console.log(typeof age)
+console.log(typeof null)
