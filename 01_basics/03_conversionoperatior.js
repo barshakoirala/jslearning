fa1d41c2b80e03357f923826cc898ad02eb62c2d
Barsha Koirala <koiralabarsha024@gmail.com>
2025-06-09 16:20:56 +0545

    basicsoperators

---
+++ 01_basics/03_conversionoperatior.js
@@ -0,0 +1,37 @@
+//let score = 33 
+
+//console.log(typeof score);
+
+//let score = "33"
+
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//let score = "Barsha"
+//console.log(typeof score);
+
+//let score = "33abcd"
+//console.log(typeof score);
+
+//let valueInNumber = Number(score);
+//console.log(valueInNumber);        // NaN
+//console.log(typeof valueInNumber); // number
+
+
+
+// now from this you can learn
+// while converting "33abcd" it will be NaN but if you want a typeof it will be string
+ 
+//let score = " "
+//console.log(typeof score); 
+
+
+//let score = " "
+//console.log(Boolean(score));  
+
+//true = 1
+//false = 0
+
+
