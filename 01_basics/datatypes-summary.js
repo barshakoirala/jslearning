a62d2b9991e6086985af6b8f6de3a354bc85165d
Barsha Koirala <koiralabarsha024@gmail.com>
2025-06-10 11:01:25 +0545

    Added datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -5,7 +5,29 @@ const isLoggedIn = false
 const outsideTemp = null 
 let userEmail;
 
-const id = symbol('123')
-const anotherId = symbol('123')
+const id = Symbol('123')
+const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
+
+
+//const bigNumber = 45637891234n
+
+console.log(typeof bigNumber);
+
+//these are  primitive datatypes 
+
+
+const heros = ["chris", "theo", "cillian"] // Arrays
+
+let myObj ={
+    name: "Barsha",
+    age: 24,
+}      // objects
+
+const myFunction = function(){
+    console.log("Hello world");
+}  //function
+
+
+// these are reference type (non primitive datatypes)
