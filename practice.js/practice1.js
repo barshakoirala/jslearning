2576d434ff0dcc7fccf431dbe89f18c248cf5acd
Barsha Koirala <koiralabarsha024@gmail.com>
2025-06-11 11:53:42 +0545

    arraypractice

--- practice.js/practice1.js
+++ practice.js/practice1.js
@@ -1,5 +1,25 @@
-const myArray = [1,2,3,4,5,6,7,8]
-const filterOdds = filteroddNumbers [myArray]
+//const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-console.log(originalArray{myArray})
-console.log(oddnumbers{filteroddNumbers});
+//const oddNumbers = numbers.filter(function(num) {
+  //return num % 2 !== 0;
+//});
+
+//console.log(oddNumbers)
+
+
+const numbers = [2,4,8,5,7]
+//let sum=0
+//let multiply = 0
+let divide = 1000
+for(let i =0;i<numbers.length;i++){
+    //console.log(numbers[i]*2)
+    //numbers[i] = numbers[i]*2
+   // sum = sum + numbers[i]
+    //multiply = multiply * [i]
+    divide = divide /numbers[i]
+
+}
+
+console.log(divide)
+
+
---
+++ practice.js/practice2.js
@@ -0,0 +1,25 @@
+//let animals = ["dog","lion","giraffe"]
+
+//let fruits = ["apple","banana","mango"]
+//console.log(fruits[1]);
+
+//fruits.shift() here shift means remove the first item
+//console.log(fruits);
+
+//let nums = [1,2,3]
+//nums.push(4)   // here push means add
+//console.log(nums);
+
+//nums.pop()
+//console.log(nums); // here pop means remove last items
+
+//nums.unshift(0) // here unahift means add items at first
+//console.log(nums);
+
+//let friends =[ "ginny", "georgia","max"]
+//for (let i=0; i < friends.length; i++ ){           // so here let i = 0 (starting from the index 0)  i < friends.length (keep going till the end )
+    //console.log ("Hi my name is" +  friends[i]);      // i++ move to next index i = i+ 1
+//}
+
+
+
