b34ac14d13219f02b7472f0158c3117475fcce07
Barsha Koirala <koiralabarsha024@gmail.com>
2025-06-18 17:39:41 +0545

    html

--- practice.js/questions.js
+++ practice.js/questions.js
@@ -1,16 +1,106 @@
 // Write a function sumEven(numbers) that takes an array and returns the sum of all even numbers.
 
-function sumEven(numbers){
-    let sum = 0
+// function sumEven(numbers){
+//     let sum = 0
 
-    for ( let num of numbers ){
-        if  ( num % 2 == 0 ){
-         sum += num;   
-        }
+//     for ( let num of numbers ){
+//         if  ( num % 2 == 0 ){
+//          sum += num;   
+//         }
             
-    }
-    return sum ;
-}
+//     }
+//     return sum ;
+// }
 
 
-console.log(sumEven([10,30,60, 75]));
+// console.log(sumEven([10,30,60, 75]));
+
+
+// Write a function reverseString(str) that returns the reverse of the input string.
+
+// function reverseString(str){ 
+
+//     let reversed = "Nepaldigitalsystem"
+
+//     for let i = str [i] 
+
+
+// }
+
+
+// let a = 45;
+//     b = 30;
+
+
+// let temp
+// temp = a;
+// a = b;
+// b = temp ;
+
+// console.log ("a is", a);
+// console.log("b is", b);
+
+
+// Write a function isPrime(n) that returns true if the number is prime, otherwise false.
+
+// function isPrime(n){
+//     if ( n <= 1 ){
+//         //  return false;   if its not prime number
+//     }
+
+//     for (let i = 2; i < n; i++) {   
+//     if (n % i === 0) {
+//       return false;   or if divisible not a prime number in this case
+// }
+//     }
+//     return true   here if theres no divisor found then its a prime number
+// }
+
+// console.log(isPrime(2));   
+// console.log(isPrime(4));
+
+
+// Write a function getValues(obj) that takes an object and returns an array of its values.
+
+
+// let person = { name: "Barsha", age: 23, city: "Kathmandu" };
+// function getValues(obj) {
+//   return Object.values(obj)
+// }
+
+// console.log(getValues(person));
+
+
+// Write a function findMax(arr) that returns the largest number?
+
+
+// function findMax(arr) {
+
+//  let max = arr[0];
+//     if (arr.length == 0) {
+//     return undefined; 
+// }
+
+// for (let i = 1; i < arr.length ;i++ ) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// return max;
+
+// }
+
+// console.log(findMax[7,8,17,29]);
+
+
+// function findMax(array){
+//     let max = 0;
+//    for (let i = 0; i < array.length; i++) {
+//         if (array[i] > max) {
+//             max = array[i];
+// }
+//    }
+//     return max ;
+//    }
+
+// console.log(findMax ([7,8,27,17]));
